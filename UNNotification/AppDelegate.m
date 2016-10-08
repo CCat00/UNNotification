ad7d8825fd3eca367fc6548a8b488fcc257dbd18
1019260351@qq.com <1019260351@qq.com>
2016-10-08 15:10:50 +0800

    UserNotificaionsDemo

--- UNNotification/AppDelegate.m
+++ UNNotification/AppDelegate.m
@@ -8,7 +8,9 @@
 
 #import "AppDelegate.h"
 
-@interface AppDelegate ()
+@import UserNotifications;
+
+@interface AppDelegate () <UNUserNotificationCenterDelegate>
 
 @end
 
@@ -17,35 +19,89 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    // 请求使用通知
+    [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:UNAuthorizationOptionBadge | UNAuthorizationOptionSound | UNAuthorizationOptionAlert completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+            //用户同意使用通知
+        }
+    }];
+    
+    //向APNS请求token
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+    
+    [[UNUserNotificationCenter currentNotificationCenter] getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+    }];
+    
+    // 设置代理
+    [[UNUserNotificationCenter currentNotificationCenter] setDelegate:self];
+    
+    /**
+     设置通知上面的交互按钮
+     UNNotificationActionOptionAuthenticationRequired //操作这个按钮会先检查iPhone是否解锁
+     UNNotificationActionOptionDestructive //按钮会被高亮标记（红色）
+     UNNotificationActionOptionForeground // 点击按钮会将APP唤起
+     */
+    UNNotificationAction *unlocking = [UNNotificationAction actionWithIdentifier:@"unlocking"
+                                                                           title:@"unlocking"
+                                                                         options:UNNotificationActionOptionAuthenticationRequired];
+    UNNotificationAction *destructive = [UNNotificationAction actionWithIdentifier:@"destructive"
+                                                                             title:@"destructive"
+                                                                           options:UNNotificationActionOptionDestructive];
+    UNNotificationAction *foreground = [UNNotificationAction actionWithIdentifier:@"foreground"
+                                                                            title:@"foreground"
+                                                                          options:UNNotificationActionOptionForeground];
+    UNTextInputNotificationAction *input = [UNTextInputNotificationAction actionWithIdentifier:@"text" title:@"text" options:UNNotificationActionOptionAuthenticationRequired textInputButtonTitle:@"text_btn" textInputPlaceholder:@"placeholder"];
+    // 注意：这里的category的标示如果和发送通知时，写的category标示不一样的时候，发过来的通知不会显示action！！！发送通知的时候可以不写标示。
+    // 我这里是，两边都写得 “ceshi”
+    UNNotificationCategory *category = [UNNotificationCategory categoryWithIdentifier:@"ceshi"
+                                                                              actions:@[unlocking, destructive, foreground, input] intentIdentifiers:@[@""]
+                                                                              options:UNNotificationCategoryOptionNone];
+    
+    [[UNUserNotificationCenter currentNotificationCenter] setNotificationCategories:[NSSet setWithObject:category]];
+    
     return YES;
 }
 
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+//请求token成功
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
 }
 
+#pragma mark - UNUserNotificationCenterDelegate
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+// The method will be called on the delegate only if the application is in the foreground. If the method is not implemented or the handler is not called in a timely manner then the notification will not be presented. The application can choose to have the notification presented as a sound, badge, alert and/or in the notification list. This decision should be based on whether the information in the notification is otherwise visible to the user.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler
+{
+    // 用户在前台使用APP的时候，收到通知，会调用此方法.
+    // 写这个回调代表，会在APP打开的情况下，弹出通知。
+    // 如果不复写次方法，或者`completionHandler();`，APP在前台收到通知将不会弹出提示。
+    completionHandler(UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert);
 }
 
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+// The method will be called on the delegate when the user responded to the notification by opening the application, dismissing the notification or choosing a UNNotificationAction. The delegate must be set before the application returns from applicationDidFinishLaunching:.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler
+{
+    //当用户是通过和通知的交互进入app的时候，拿到通知对象。
+    UNNotificationContent *content = response.notification.request.content;
+    
+    if ([content.categoryIdentifier isEqualToString:@"ceshi"]) {
+        
+        if ([response.actionIdentifier isEqualToString:@"text"]) {
+            UNTextInputNotificationResponse *textResp = (UNTextInputNotificationResponse *)response;
+            NSLog(@"输入文字的内容是%@", textResp.userText);
+        }
+        else if ([response.actionIdentifier isEqualToString:@"unlocking"]) {
+            NSLog(@"unlocking");
+        }
+        else if ([response.actionIdentifier isEqualToString:@"destructive"]) {
+            NSLog(@"destructive");
+        }
+        else if ([response.actionIdentifier isEqualToString:@"foreground"]) {
+            NSLog(@"foreground");
+        }
+    }
+    
+    completionHandler();
 }
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
-
 
 @end
--- UNNotification/ViewController.m
+++ UNNotification/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+@import UserNotifications;
 
 @interface ViewController ()
 
@@ -17,6 +18,72 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"%s, %d", __func__ ,__LINE__);
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    [super touchesBegan:touches withEvent:event];
+    
+    // 创建通知
+    UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+    content.title = @"这是一条通知的标题";
+    content.body = @"这是一条通知的内容";
+    content.categoryIdentifier = @"ceshi";
+    
+    // 创建发送触发
+    UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:5 repeats:NO];
+    
+    // 发送请求标识符
+    NSString *identifier = @"com.hanwei.firstNotificationIndentifier";
+    
+    // 创建一个通知请求
+    UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
+    
+    
+    // 将请求添加到通知中心
+    [[UNUserNotificationCenter currentNotificationCenter] addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+        if (!error) {
+            NSLog(@"通知添加成功");
+        }
+    }];
+    
+//    // 不管通知是否已经展示过，都可以根据标示进行通知的更新
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(8 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        
+////        NSLog(@"两秒钟后更新通知内容");
+//        
+//        // 创建发送触发
+//        UNTimeIntervalNotificationTrigger *trigger = [UNTimeIntervalNotificationTrigger triggerWithTimeInterval:2 repeats:NO];
+//        
+//        UNMutableNotificationContent *content = [[UNMutableNotificationContent alloc] init];
+//        content.title = @"这是一条通知的标题";
+//        content.body = @"这是新的内容😀";
+//        content.categoryIdentifier = @"ceshi";
+//        
+//        // 创建一个通知请求
+//        // 同一个标示的话就会覆盖之前的通知
+//        UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:identifier content:content trigger:trigger];
+//        
+//        [[UNUserNotificationCenter currentNotificationCenter] addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+//            if (!error) {
+//                NSLog(@"新的通知添加成功");
+//            }
+//        }];
+//    });
+    
+    
+//    // 移除展示过的通知
+//    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(6 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+//        [[UNUserNotificationCenter currentNotificationCenter] removeDeliveredNotificationsWithIdentifiers:@[identifier]];
+//        
+//    });
+//    
+//     //移除还未展示的通知
+//    [[UNUserNotificationCenter currentNotificationCenter] removePendingNotificationRequestsWithIdentifiers:@[identifier]];
+    
+    
+    
 }
 
 
